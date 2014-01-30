34d30d9dce8dd27f02a43d6c4cfadf03db3b02dc
Andrew Ning <andrew.ning@nrel.gov>
2014-01-30 15:37:00 -0700

    added BasicAEP component and corresponding gradient unit test

--- src/Plant_AEPSE/Basic_AEP/basic_aep.py
+++ src/Plant_AEPSE/Basic_AEP/basic_aep.py
@@ -22,9 +22,9 @@ class aep_weibull_assembly(GenericAEPModel):
     k = Float(iotype='in', desc='shape or form factor')
     wind_curve = Array(iotype='in', units='m/s', desc='wind curve')
     power_curve = Array(iotype='in', units='W', desc='power curve (power)')
-    
+
     # parameters
-    array_losses = Float(0.059, iotype='in', desc = 'energy losses due to turbine interactions - across entire plant') 
+    array_losses = Float(0.059, iotype='in', desc = 'energy losses due to turbine interactions - across entire plant')
     other_losses = Float(0.0, iotype='in', desc = 'energy losses due to blade soiling, electrical, etc')
     availability = Float(0.94, iotype='in', desc = 'average annual availbility of wind turbines at plant')
     turbine_number = Int(100, iotype='in', desc = 'total number of wind turbines at the plant')
@@ -75,7 +75,7 @@ class WeibullCDF(CDFBase):
     k = Float(iotype='in', desc='shape or form factor')
 
     def __init__(self):
-        
+
         super(WeibullCDF,self).__init__()
 
         #controls what happens if derivatives are missing
@@ -88,19 +88,19 @@ class WeibullCDF(CDFBase):
         self.d_F_d_x = np.diag(- np.exp(-(self.x/self.A)**self.k) * (1./self.A) * (-self.k * ((self.x/self.A)**(self.k-1.0))))
         self.d_F_d_A = - np.exp(-(self.x/self.A)**self.k) * (1./self.x) * (self.k * ((self.A/self.x)**(-self.k-1.0)))
         self.d_F_d_k = - np.exp(-(self.x/self.A)**self.k) * -(self.x/self.A)**self.k * np.log(self.x/self.A)
-    
+
     def list_deriv_vars(self):
 
         inputs = ['x', 'A', 'k']
         outputs = ['F']
-        
+
         return inputs, outputs
 
-    
+
     def provideJ(self):
-        
-        self.J = hstack((self.d_F_d_x, self.d_F_d_A, self.d_F_d_k))        
-        
+
+        self.J = hstack((self.d_F_d_x, self.d_F_d_A, self.d_F_d_k))
+
         return self.J
 
 
@@ -108,11 +108,11 @@ class RayleighCDF(CDFBase):
     """Rayleigh cumulative distribution function"""
 
     xbar = Float(iotype='in', desc='mean value of distribution')
-    
+
     def __init__(self):
-      
+
         super(RayleighCDF,self).__init__()
-        
+
         #controls what happens if derivatives are missing
         self.missing_deriv_policy = 'assume_zero'
 
@@ -124,17 +124,54 @@ class RayleighCDF(CDFBase):
         self.d_F_d_xbar = - np.exp(-np.pi/4.0*(self.x/self.xbar)**2) * ((np.pi/2.0)*(self.xbar/self.x)**(-3)) * (1.0 / self.x)
 
     def list_deriv_vars(self):
-        
+
         inputs = ['x', 'xbar']
         outputs = ['F']
 
         return inputs, outputs
-    
+
     def provideJ(self):
-        
+
         self.J = hstack((self.d_F_d_x, self.d_F_d_xbar))
-        
+
         return self.J
+
+
+
+class BasicAEP(Component):
+
+    # in
+    AEP_one_turbine = Float(iotype='in', units='kW*h')
+
+    # parameters
+    array_losses = Float(0.059, iotype='in', desc='energy losses due to turbine interactions - across entire plant')
+    other_losses = Float(0.0, iotype='in', desc='energy losses due to blade soiling, electrical, etc')
+    availability = Float(0.94, iotype='in', desc='average annual availbility of wind turbines at plant')
+    turbine_number = Int(100, iotype='in', desc='total number of wind turbines at the plant')
+
+    # outputs
+    gross_aep = Float(iotype='out', desc='Gross Annual Energy Production before availability and loss impacts', units='kW*h')
+    net_aep = Float(iotype='out', desc='Net Annual Energy Production after availability and loss impacts', units='kW*h')
+
+    missing_deriv_policy = 'assume_zero'
+
+    def execute(self):
+
+        self.gross_aep = self.turbine_number * self.AEP_one_turbine
+        self.net_aep = self.availability * (1-self.array_losses) * (1-self.other_losses) * self.gross_aep
+
+    def list_deriv_vars(self):
+
+        inputs = ('AEP_one_turbine',)
+        outputs = ('gross_aep', 'net_aep')
+
+        return inputs, outputs
+
+    def provideJ(self):
+
+        J = np.array([[self.turbine_number],  [self.availability * (1-self.array_losses) * (1-self.other_losses) * self.turbine_number]])
+
+        return J
 
 
 class aep_component(Component):
@@ -150,12 +187,12 @@ class aep_component(Component):
     availability = Float(0.94, iotype='in', desc='average annual availbility of wind turbines at plant')
     turbine_number = Int(100, iotype='in', desc='total number of wind turbines at the plant')
 
-    # outputs 
+    # outputs
     gross_aep = Float(iotype='out', desc='Gross Annual Energy Production before availability and loss impacts', unit='kWh')
-    net_aep = Float(iotype='out', desc='Net Annual Energy Production after availability and loss impacts', unit='kWh') 
+    net_aep = Float(iotype='out', desc='Net Annual Energy Production after availability and loss impacts', unit='kWh')
 
     def __init__(self):
-        
+
         super(aep_component,self).__init__()
 
         #controls what happens if derivatives are missing
@@ -165,14 +202,14 @@ class aep_component(Component):
 
         self.gross_aep = self.turbine_number * np.trapz(self.power_curve, self.CDF_V)*365.0*24.0  # in kWh
         self.net_aep = self.availability * (1-self.array_losses) * (1-self.other_losses) * self.gross_aep
- 
+
     def list_deriv_vars(self):
 
         inputs = ['CDF_V', 'power_curve']
         outputs = ['gross_aep', 'net_aep']
-        
+
         return inputs, outputs
-    
+
     def provideJ(self):
 
         P = self.power_curve
@@ -200,9 +237,9 @@ class aep_component(Component):
         self.J[0, 0:n] = d_gross_d_cdf
         self.J[0, n:2*n] = d_gross_d_p
         self.J[1, 0:n] = d_net_d_cdf
-        self.J[1, n:2*n] = d_net_d_p        
+        self.J[1, n:2*n] = d_net_d_p
         #self.J[0, 2*n] = dAEP_dlossFactor
- 
+
         return self.J
 
 def example():
@@ -210,7 +247,7 @@ def example():
     aeptest = aep_weibull_assembly()
 
     #print aeptest.aep.machine_rating
-    
+
     aeptest.wind_curve = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, \
                            11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0, 26.0])
     aeptest.power_curve = np.array([0.0, 0.0, 0.0, 0.0, 187.0, 350.0, 658.30, 1087.4, 1658.3, 2391.5, 3307.0, 4415.70, \
